2230f4f8a119edf58e9c510120623ad75342d993
Moises Piva da Costa <moisespiva9492@gmail.com>
2025-10-24 01:28:39 -0300

    $commitMessage

---
+++ atualização.bat
@@ -0,0 +1,14 @@
+# Navega até o diretório do repositório (ajuste conforme necessário)
+Set-Location "C:\Users\moises.costa\Desktop\RepositorioProjetos Sistemas"
+
+# Adiciona todos os arquivos modificados
+git add .
+
+# Solicita uma mensagem de commit ao usuário
+$commitMessage = Read-Host "Sucesso"
+
+# Realiza o commit com a mensagem fornecida
+git commit -m "$commitMessage"
+
+# Envia as alterações para o repositório remoto (branch principal)
+git push origin main
